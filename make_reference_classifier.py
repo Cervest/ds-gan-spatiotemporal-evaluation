914a358a52ce20b95bb76fadb5b6d98eaebdcf3d
shahine <shahine96@gmail.com>
2020-08-28 19:12:48 +0100

    changes make reference classifier script

--- make_reference_classifier.py
+++ make_reference_classifier.py
@@ -28,18 +28,20 @@ def main(args, cfg):
 
     # Retrieve dataloaders of annotated target frames from testing set
     logging.info("Loading testing set")
-    test_loader = make_annotated_clean_frames_dataloaders(experiment)
+    train_loader, val_loader = make_annotated_clean_frames_dataloaders(experiment)
 
     # Convert into (n_pixel, n_channel), (n_pixel,) arrays for sklearn
     logging.info("Converting datasets to arrays")
-    X_test, y_test = dataset_as_arrays(test_loader, seed=cfg['experiment']['seed'])
+    X_train, y_train = dataset_as_arrays(train_loader, seed=cfg['experiment']['seed'])
+    X_val, y_val = dataset_as_arrays(val_loader, seed=cfg['experiment']['seed'])
 
     # Remove background pixels which we are not interested in classifying
-    X_test, y_test = filter_background_pixels(X_test, y_test)
+    X_train, y_train = filter_background_pixels(X_train, y_train)
+    X_val, y_val = filter_background_pixels(X_val, y_val)
 
     # Fit classifier to testing set
     classifier_cfg = cfg['reference_classifier']
-    rf = fit_classifier_by_chunks(X_train=X_test, y_train=y_test,
+    rf = fit_classifier_by_chunks(X_train=X_train, y_train=y_train,
                                   l2_weight=classifier_cfg['l2_weight'],
                                   n_chunks=classifier_cfg['n_chunks'],
                                   tol=classifier_cfg['tol'],
@@ -53,11 +55,11 @@ def main(args, cfg):
 
     # Compute and save accuracy
     logging.info("Computing accuracy on testing set")
-    compute_and_save_accuracy(X_test, y_test, rf, dump_path)
+    compute_and_save_accuracy(X_val, y_val, rf, dump_path)
 
     # Compute and save confusion matrix
     logging.info("Computing confusion matrix on testing set")
-    compute_and_save_confusion_matrix(X_test, y_test, rf, dump_path)
+    compute_and_save_confusion_matrix(X_val, y_val, rf, dump_path)
 
 
 def make_annotated_clean_frames_dataloaders(experiment):
@@ -82,11 +84,16 @@ def make_annotated_clean_frames_dataloaders(experiment):
                               attribute_name='annotation_transform')
 
     # Build dataloaders restricted to corresponding indices sets
-    test_indices = experiment.test_set.indices
-    test_loader = make_dataloader_from_indices(dataset=target_annotated_frames_dataset,
-                                               batch_size=horizon,
-                                               indices=test_indices)
-    return test_loader
+    train_indices = experiment.train_set.indices
+    train_loader = make_dataloader_from_indices(dataset=target_annotated_frames_dataset,
+                                                batch_size=horizon,
+                                                indices=train_indices)
+
+    val_indices = experiment.val_set.indices
+    val_loader = make_dataloader_from_indices(dataset=target_annotated_frames_dataset,
+                                              batch_size=horizon,
+                                              indices=val_indices)
+    return train_loader, val_loader
 
 
 def set_transform_recursively(concat_dataset, transform, attribute_name):
