1be0fa4722b7b7aa55e5d332cf84716cf506ffc6
shahine <shahine96@gmail.com>
2020-08-31 13:26:16 +0100

    rewrite other repro

--- repro/experiments/cloud_removal/cgan_upper_bound_toy/.dvc_run_testing.sh
+++ repro/experiments/cloud_removal/cgan_upper_bound_toy/.dvc_run_testing.sh
@@ -17,15 +17,29 @@ CONFIG=src/rsgan/config/cloud_removal/cgan_upper_bound_toy.yaml
 EXPERIMENT=src/rsgan/experiments/cloud_removal/cgan_toy_cloud_removal.py
 DATASET=data/toy/cloud_removal
 ROOT=data/experiments_outputs/cgan_upper_bound_toy
-TRAIN_DIR=$ROOT/dvc_run/run/
-TEST_DIR=$ROOT/dvc_run/eval/
+SEEDS=(17 37 43 73 101)
+CHKPTS=("seed_17/checkpoints/epoch=42.ckpt"
+        "seed_37/checkpoints/epoch=62.ckpt"
+        "seed_43/checkpoints/epoch=60.ckpt"
+        "seed_73/checkpoints/epoch=63.ckpt"
+        "seed_101/checkpoints/epoch=61.ckpt")
+
 
 # Run dvc pipeline on specified device
-dvc run -v -n test_cgan_toy_upper_bound \
--d $CONFIG \
--d $EXPERIMENT \
--d $DATASET \
--d $TRAIN_DIR \
--o $TEST_DIR \
-"python make_reference_classifier.py --cfg=$CONFIG --o=$TEST_DIR/reference_classifier/classifier.pickle \
-  && python run_testing.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE"
+for (( i=0; i<${#SEEDS[*]}; ++i));
+do
+  NAME=seed_${SEEDS[$i]}
+  CHKPT=$ROOT/${CHKPTS[$i]}
+  TRAIN_DIR=$ROOT/$NAME/run
+  TEST_DIR=$ROOT/$NAME/eval
+  CLASSIFIER=$TEST_DIR/reference_classifier/classifier.pickle
+  dvc run -v -f -n test_cgan_toy_upper_bound_$NAME \
+  -d $CONFIG \
+  -d $EXPERIMENT \
+  -d $DATASET \
+  -d $TRAIN_DIR \
+  -d $CHKPT \
+  -o $TEST_DIR \
+  "python make_reference_classifier.py --cfg=$CONFIG --o=$CLASSIFIER \
+    && python run_testing.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --chkpt=$CHKPT --classifier=$CLASSIFIER"
+done
--- repro/experiments/cloud_removal/cgan_upper_bound_toy/.dvc_run_training.sh
+++ repro/experiments/cloud_removal/cgan_upper_bound_toy/.dvc_run_training.sh
@@ -17,13 +17,17 @@ CONFIG=src/rsgan/config/cloud_removal/cgan_upper_bound_toy.yaml
 EXPERIMENT=src/rsgan/experiments/cloud_removal/cgan_toy_cloud_removal.py
 DATASET=data/toy/cloud_removal
 ROOT=data/experiments_outputs/cgan_upper_bound_toy
-TRAIN_DIR=$ROOT/dvc_run/run/
 
 
 # Run dvc pipeline on specified device
-dvc run -v -n train_cgan_toy_upper_bound \
--d $CONFIG \
--d $EXPERIMENT \
--d $DATASET \
--o $TRAIN_DIR \
-"python run_training.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --experiment_name=dvc_run"
+for SEED in 17 37 43 73 101 ;
+do
+  NAME=seed_$SEED
+  TRAIN_DIR=$ROOT/$NAME/run
+  dvc run -v -f -n train_cgan_toy_upper_bound_$NAME \
+  -d $CONFIG \
+  -d $EXPERIMENT \
+  -d $DATASET \
+  -o $TRAIN_DIR \
+  "python run_training.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --experiment_name=$NAME --seed=$SEED"
+done
