f8467050daf719ccefe044f3f2cccfda95bb6aee
shahine <shahine96@gmail.com>
2020-09-02 12:30:49 +0100

    changes testing checkpoints

--- repro/experiments/cloud_removal/cgan_frame_recurrent_toy/.dvc_run_testing.sh
+++ repro/experiments/cloud_removal/cgan_frame_recurrent_toy/.dvc_run_testing.sh
@@ -18,11 +18,11 @@ EXPERIMENT=src/rsgan/experiments/cloud_removal/cgan_frame_recurrent_toy_cloud_re
 DATASET=data/toy/cloud_removal
 ROOT=data/experiments_outputs/cgan_frame_recurrent_toy_cloud_removal
 SEEDS=(17 37 43 73 101)
-CHKPTS=("seed_17/checkpoints/epoch=42.ckpt"
-        "seed_37/checkpoints/epoch=62.ckpt"
-        "seed_43/checkpoints/epoch=60.ckpt"
-        "seed_73/checkpoints/epoch=63.ckpt"
-        "seed_101/checkpoints/epoch=61.ckpt")
+CHKPTS=("seed_17/checkpoints/epoch=62.ckpt"
+        "seed_37/checkpoints/epoch=59.ckpt"
+        "seed_43/checkpoints/epoch=59.ckpt"
+        "seed_73/checkpoints/epoch=61.ckpt"
+        "seed_101/checkpoints/epoch=59.ckpt")
 
 
 # Run dvc pipeline on specified device
